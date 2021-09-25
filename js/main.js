dac11bb383ec3fc1d64c60953dc9a259e86e99ef
Pifan91 <nepifanov91@gmail.com>
2021-09-25 20:37:41 +0300

    2 задачи в одной функции

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function getNumberFromRange(minNumber, maxNumber, decimalPlaces) {
+  if (minNumber < 0 || maxNumber < 0) {
+    return;
+  }
+
+  if (minNumber > maxNumber) {
+    const tempNumber = maxNumber;
+    maxNumber = minNumber;
+    minNumber = tempNumber;
+  }
+
+  if (!decimalPlaces) {
+    decimalPlaces = 0;
+  }
+
+  function getRandomInt(min, max, decimal) {
+    return (Math.random() * (max - min) + min).toFixed(decimal);
+  }
+
+  getRandomInt(minNumber, maxNumber, decimalPlaces);
+}
+
+getNumberFromRange(50, 40);
+getNumberFromRange(9, 3, 4);
